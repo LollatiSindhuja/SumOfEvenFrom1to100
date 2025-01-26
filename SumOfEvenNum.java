c2c895f050da5bca4e4059ba8068b8ec63199ed3
LollatiSindhuja <sindhujalollati@gmail.com>
2025-01-26 18:57:46 +0530

    Add files via upload

---
+++ SumOfEvenNum.java
@@ -0,0 +1,15 @@
+public class SumOfEvenNum {
+    public static void main(String[] args) {
+    int num = 1;
+    int sum = 0;
+
+    while (num <= 100) {
+        if (num % 2 == 0) {
+            sum += num;
+        }
+        num++;
+    }
+
+    System.out.println("Sum of all even numbers between 1 and 100 is: " + sum);
+}
+}
